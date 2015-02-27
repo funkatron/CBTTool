0e0254727a2bc4ca2890d7f96c7b85d50bc15bf8
Ed Finkler <coj@funkatron.com>
2015-02-27 14:11:38 -0500

    dev-server.sh will make DB file if needed

--- dev-server.sh
+++ dev-server.sh
@@ -2,6 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SERVER_ADDR="0.0.0.0:9909"
 
+# making sqlite3 file if needed
+sqlite3 ./data/cbttool.sqlite3 "select ''";
+
 cd "$DIR/www"
 
 echo "visit http://$SERVER_ADDR"
