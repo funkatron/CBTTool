77e2935d7841882755d52446aa2497772a5af95c
Ed Finkler <coj@funkatron.com>
2015-03-02 10:34:47 -0500

    run migrations on dev server startup

--- dev-server.sh
+++ dev-server.sh
@@ -5,6 +5,10 @@ SERVER_ADDR="0.0.0.0:9909"
 # making sqlite3 file if needed
 sqlite3 ./data/cbttool.sqlite3 "select ''";
 
+# run migrations
+echo "running migrations"
+./vendor/bin/phinx migrate --environment='development'
+
 cd "$DIR/www"
 
 echo "visit http://$SERVER_ADDR"
